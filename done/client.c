e6af1101b1dcc339ad8c9fd7cf7c5ba7bb7afd83
Unknown <simon.wicky@epfl.ch>
2018-03-15 13:14:45 +0100

    updated pointer thingy

--- done/client.c
+++ done/client.c
@@ -1,11 +1,18 @@
 #include "client.h"
+#include "node.h"
+#include "system.h"
 
 
 void client_end(client_t *client){
-	client.node.end();
+	node_end(&client->node);
 }
 
 error_code client_init(client_init_args_t client_init){
-	client_init.client.name = client_init.name;
-	//wip
+	client_t* client = client_init.client;
+	client->name = client_init.name;
+	client->socket = get_socket(1);
+	//node_init(client.&node, ???);
+
+	return ERR_BAD_PARAMETER;
+	
 }
--- done/client.h
+++ done/client.h
@@ -11,9 +11,9 @@
 
 #include "error.h"     // for error_code type
 #include "node.h"      // in week 5
-#include "node_list.h" // weeks 6 to 10
-#include "args.h"      // weeks 10 and after
-#include "ring.h"      // weeks 11 and after
+//#include "node_list.h" // weeks 6 to 10
+//#include "args.h"      // weeks 10 and after
+//#include "ring.h"      // weeks 11 and after
 
 /**
  * @brief client state
