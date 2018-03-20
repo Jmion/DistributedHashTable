723ba1f1e524884ba24a38ff02b0bfaa9facd8a0
Unknown <simon.wicky@epfl.ch>
2018-03-20 11:45:45 +0100

    tried pps-client

---
+++ done/pps-client-get.c
@@ -0,0 +1,34 @@
+//
+// Created by Simon Wicky on 20.03.18.
+//
+
+#include "client.h"
+#include "node.h"
+#include "network.h"
+#include <stdio.h>
+
+
+int main(void){
+    client_init_args_t init_client;
+    init_client.name = "client";
+    client_init(init_client);
+    client_t* client = init_client.client;
+
+    do{
+        char key;
+        scanf("%c", &key);
+        int value;
+        error_code error = network_get(*client, key, &value);
+        if (error != 0){
+            printf("FAIL\n");
+        } else {
+            printf("OK %d\n",value);
+        }
+
+    }while(!feof(stdin) && ! ferror(stdin));
+
+
+
+
+    return 0;
+}
---
+++ done/pps-client-put.c
@@ -0,0 +1,35 @@
+//
+// Created by Simon Wicky on 20.03.18.
+//
+
+#include "client.h"
+#include "node.h"
+#include "network.h"
+#include <stdio.h>
+
+
+int main(void){
+    client_init_args_t init_client;
+    init_client.name = "client";
+    client_init(init_client);
+    client_t* client = init_client.client;
+
+    do{
+        char key;
+        scanf("%c", &key);
+        int value;
+        scanf("%d", &value);
+        error_code error = network_put(*client, key, value);
+        if (error != 0){
+            printf("FAIL\n");
+        } else {
+            printf("OK\n");
+        }
+
+    }while(!feof(stdin) && ! ferror(stdin));
+
+
+
+
+    return 0;
+}
