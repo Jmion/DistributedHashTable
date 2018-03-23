ebd2b385d123dd2f77b67e2538d55cbbafd785da
Unknown <jeremy.mion@yahoo.ca>
2018-03-23 10:22:15 +0100

    Done network v1
    Still need to add debig messages

--- done/network.c
+++ done/network.c
@@ -10,6 +10,13 @@
 struct sockaddr_in srv_addr;
 
 //MODULARISATION
+/**
+ *
+ * @param client
+ * @param msg
+ * @param size
+ * @return -1 in case of error
+ */
 ssize_t send(client_t client, const void* msg, size_t size){
 	return sendto(client.socket, msg, size, 0, &client.node, sizeof(client.node));
 }
@@ -27,9 +34,16 @@ ssize_t receive_from_server(client_t client, void* buffer, size_t size){
 //*******END OF MODULARISATION*****
 
 error_code network_get(client_t client, pps_key_t key, pps_value_t *value){
-
-    //char out_msg = htonl(key);
-    network_send(client,&out_msg);
+	//send get request
+	if(send(client,&key, sizeof(key)) == -1){
+		//TODO error message
+		return ERR_NETWORK;
+	}
+	//receive response
+	if (receive_from_server(client,value, sizeof(*value)) != sizeof(*value)){
+		//TODO error message
+		return ERR_NETWORK;
+	}
 };
 
 error_code network_put(client_t client, pps_key_t key, pps_value_t value){
@@ -60,41 +74,3 @@ error_code network_put(client_t client, pps_key_t key, pps_value_t value){
 error_code network_del(client_t client, pps_key_t key){
     return ERR_NONE;
 };
-
-
-
-
-/*error_code network_send(client_t client, char const* out_msg){
-    struct sockaddr_in srv_addr = client.node.address;
-    if (sendto(client.socket, &out_msg, sizeof(out_msg), 0,
-               (struct sockaddr *) &srv_addr, sizeof(srv_addr)) == -1)
-        die("sendto");
-    return ERR_NONE;
-}*/
-
-
-/*
-error_code network_receive(){
-    char in_msg[RETURN_MSG_LENGTH];
-    ssize_t in_msg_len = rec(s, &in_msg, sizeof(in_msg), 0);
-    if (in_msg_len == RETURN_MSG_LENGTH || in_msg_len == 0) { // Valid response.
-        // Parse response with ntohl
-        char response_msg[RETURN_MSG_LENGTH];
-        (void)memset(&response_msg,0, sizeof(response_msg));
-        for(int i = 0; i < RETURN_MSG_LENGTH; ++i){
-            response_msg[i] = ntohl(in_msg[i]);
-        }
-        if(in_msg_len == 0){
-            return ERR_NONE;
-        }else if(in_msg_len == RETURN_MSG_LENGTH){
-
-        }
-
-    } else if (in_msg_len == -1) { // Timeout.
-        M_EXIT_ERR(ERR_NETWORK,"timeout");
-    } else { // Wrong message size.
-        puts("Received invalid response.");
-    }
-}
-
-*/
