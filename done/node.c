b86cd2b59ddc2f23bc6667a15c941174e58d1ebe
Simon Wicky <simon.wicky@epfl.ch>
2018-04-16 10:08:13 +0200

    changes on node structure

--- done/node.c
+++ done/node.c
@@ -3,16 +3,23 @@
 #include "node.h"
 #include "util.h"
 #include "system.h"
+#include <string.h>
+#include <stdlib.h>
 
 
 
 error_code node_init(node_t *node, const char *ip, uint16_t port, size_t node_id){
-	node->ip = ip;
+	char* ip_copied = calloc(strlen(ip), sizeof(char));
+	if (ip_copied == NULL){
+		return ERR_NOMEM;
+	}
+	strncpy(ip_copied, ip, strlen(ip));
+	node->ip = ip_copied;
 	node->port = port;
 	node->id = node_id; 
 	return get_server_addr(ip, port,&node->address);
 }
 
 void node_end(node_t *node){
-	
+	free_const_ptr(node->ip);
 }
