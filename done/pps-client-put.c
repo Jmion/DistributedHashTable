a570e5e03d5a8013adcd334360ec10b800f4d91d
Unknown <jeremy.mion@yahoo.ca>
2018-04-18 09:16:49 +0200

    FIx seg fault. program will halt if error init client

--- done/pps-client-put.c
+++ done/pps-client-put.c
@@ -8,38 +8,38 @@
 #include <stdio.h>
 #include "node_list.h"
 #include "config.h"
+#include "error.h"
 
 
-int main(void){
-    client_init_args_t init_client;
-    client_t cl;
-    init_client.client = &cl;
-    init_client.name = "client";
-    init_client.nodes_list = get_nodes();
-    client_init(init_client);
-    client_t* client = init_client.client;
+int main(void) {
+	client_init_args_t init_client;
+	client_t cl;
+	init_client.client = &cl;
+	init_client.name = "client";
+	init_client.nodes_list = get_nodes();
+	error_code client_init_err = client_init(init_client);
+	client_t *client = init_client.client;
 
-    do{
-        char key[MAX_MSG_ELEM_SIZE+1];
-        int read = scanf("%s", key);
-        char value[MAX_MSG_ELEM_SIZE+1];
-        read += scanf("%s", value);
+	M_EXIT_IF_ERR(client_init_err, "Error initializing client");
+	do {
+		char key[MAX_MSG_ELEM_SIZE + 1];
+		int read = scanf(MAX_MSG_ELEM_SCANF, key);
+		char value[MAX_MSG_ELEM_SIZE + 1];
+		read += scanf(MAX_MSG_ELEM_SCANF, value);
 
-        if (read == 2) {
-            error_code error = network_put(*client, key, value);
-            if (error != 0){
-                printf("FAIL\n");
-            } else {
-                printf("OK\n");
-            }
-        }
+		if (read == 2) {
+			error_code error = network_put(*client, key, value);
+			if (error != 0) {
+				printf("FAIL\n");
+			} else {
+				printf("OK\n");
+			}
+		}
 
-        while(!feof(stdin) && ! ferror(stdin) && getc(stdin) != '\n');
+		while (!feof(stdin) && !ferror(stdin) && getc(stdin) != '\n');
 
-    }while(!feof(stdin) && ! ferror(stdin));
-
-
+	} while (!feof(stdin) && !ferror(stdin));
 
 
-    return 0;
+	return 0;
 }
