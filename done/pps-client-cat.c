7cbe91ec279874b58ecc49bf2954ab747e89d7f9
Simon Wicky <simon.wicky@epfl.ch>
2018-04-23 19:45:31 +0200

    pps-client cat et substr

--- done/pps-client-cat.c
+++ done/pps-client-cat.c
@@ -9,6 +9,7 @@
 #include "node_list.h"
 #include "config.h"
 #include "error.h"
+#include <string.h>
 
 
 int main(void) {
@@ -19,27 +20,40 @@ int main(void) {
 	init_client.nodes_list = get_nodes();
 	error_code client_init_err = client_init(init_client);
 	client_t *client = init_client.client;
+	char value[MAX_MSG_ELEM_SIZE + 1];
+	memset(value, 0, MAX_MSG_ELEM_SIZE + 1);
+	size_t value_len = 0;
 
 	M_EXIT_IF_ERR(client_init_err, "Error initializing client");
+		char key1[MAX_MSG_ELEM_SIZE + 1];
+		scanf(MAX_MSG_ELEM_SCANF, key1);
 	do {
-		char key[MAX_MSG_ELEM_SIZE + 1];
-		int read = scanf(MAX_MSG_ELEM_SCANF, key);
-		char value[MAX_MSG_ELEM_SIZE + 1];
-		read += scanf(MAX_MSG_ELEM_SCANF, value);
-
-		// if (read == 2) {
-		// 	error_code error = network_put(*client, key, value);
-		// 	if (error != 0) {
-		// 		printf("FAIL\n");
-		// 	} else {
-		// 		printf("OK\n");
-		// 	}
-		// }
+		char key2[MAX_MSG_ELEM_SIZE + 1];
+		int read = scanf(MAX_MSG_ELEM_SCANF, key2);
+		 if (read == 1) {
+		 	pps_value_t value_get;
+		 	error_code error = network_get(*client, key1, &value_get);
+		 	if (error != 0) {
+		 		printf("FAIL\n");
+		 		return 0;
+		 	} 
+			strcpy(&value[value_len], value_get);
+		 	value_len = strlen(value);
+		}
+		strcpy(key1, key2);
+		printf("%s\n", value);
 
 		while (!feof(stdin) && !ferror(stdin) && getc(stdin) != '\n');
 
 	} while (!feof(stdin) && !ferror(stdin));
 
+	error_code error = network_put(*client, key1, value);
+
+ 	if (error == 0) {
+		printf("OK\n");
+	} else {
+		printf("FAIL\n");
+	};
 
 	return 0;
 }
--- done/pps-client-substr.c
+++ done/pps-client-substr.c
@@ -11,35 +11,86 @@
 #include "error.h"
 
 
-int main(void) {
-	client_init_args_t init_client;
-	client_t cl;
-	init_client.client = &cl;
-	init_client.name = "client";
-	init_client.nodes_list = get_nodes();
-	error_code client_init_err = client_init(init_client);
-	client_t *client = init_client.client;
-
-	M_EXIT_IF_ERR(client_init_err, "Error initializing client");
-	do {
-		char key[MAX_MSG_ELEM_SIZE + 1];
-		int read = scanf(MAX_MSG_ELEM_SCANF, key);
-		char value[MAX_MSG_ELEM_SIZE + 1];
-		read += scanf(MAX_MSG_ELEM_SCANF, value);
-
-		// if (read == 2) {
-		// 	error_code error = network_put(*client, key, value);
-		// 	if (error != 0) {
-		// 		printf("FAIL\n");
-		// 	} else {
-		// 		printf("OK\n");
-		// 	}
-		// }
-
-		while (!feof(stdin) && !ferror(stdin) && getc(stdin) != '\n');
-
-	} while (!feof(stdin) && !ferror(stdin));
+int main(void){
+    client_init_args_t init_client;
+    client_t cl;
+    init_client.client = &cl;
+    init_client.name = "client";
+    init_client.nodes_list = get_nodes();
+    error_code errCode = client_init(init_client);
+    M_EXIT_IF_ERR(errCode,"Error initializing client");
+    client_t* client = init_client.client;
 
 
-	return 0;
+    //getting key
+    do{
+	    char key1[MAX_MSG_ELEM_SIZE+1];
+	    pps_value_t value;
+	    if (scanf("%s", key1) == 1){
+
+	        error_code error = network_get(*client, key1, &value);
+
+	        if (error != ERR_NONE){
+	            printf("FAIL\n");
+	            return 0;
+	        }
+
+
+	        while(!feof(stdin) && ! ferror(stdin) && getc(stdin) != '\n');
+
+	    } else {
+	    	return 0;
+	    }
+	    //getting position
+	    ssize_t position;
+	    if (scanf("%zu", &position) == 1) {
+	    if (position < 0) {
+	    	position += strlen(value);    	
+	    }
+	    	if (position >= strlen(value)) {
+	    		printf("FAIL\n");
+	            return 0;
+	    	}
+	    } else {
+	    	return 0;
+	    }
+	    	while(!feof(stdin) && ! ferror(stdin) && getc(stdin) != '\n');
+
+
+	    //getting length
+	    size_t length;
+	    if (scanf("%zu", &length) == 1) {
+	    	if (position + length >= strlen(value)) {
+	    		printf("FAIL\n");
+	            return 0;
+	    	}
+	    } else {
+	    	return 0;
+	    }
+	    	while(!feof(stdin) && ! ferror(stdin) && getc(stdin) != '\n');	
+
+	    char key2[MAX_MSG_ELEM_SIZE+1];
+	    if (scanf("%s", key2) ==  1){
+
+	        char value2[MAX_MSG_ELEM_SIZE+1];
+	        strncpy(value2, &value[position], length);
+	        pps_value_t value_get2 = (pps_value_t) value2;
+	        error_code error = network_put(*client, key2, value2);
+
+	        if (error != ERR_NONE){
+	            printf("FAIL\n");
+	            return 0;
+	        }
+
+
+
+	        while(!feof(stdin) && ! ferror(stdin) && getc(stdin) != '\n');
+
+	    	printf("OK\n");
+	    } else {
+	    	return 0;
+	    }
+	} while(!feof(stdin) && ! ferror(stdin));
+
+    return 0;
 }
