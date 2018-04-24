6054696b1410700bc3c16107844296a7b12c8740
Simon Wicky <simon.wicky@epfl.ch>
2018-04-24 09:53:26 +0200

    corrected find

--- done/pps-client-find.c
+++ done/pps-client-find.c
@@ -23,40 +23,29 @@ int main(void) {
 	M_EXIT_IF_ERR(client_init_err, "Error initializing client");
 	do {
 		char key1[MAX_MSG_ELEM_SIZE + 1];
+		char value1[MAX_MSG_ELEM_SIZE + 1];
+		char value2[MAX_MSG_ELEM_SIZE + 1];
 		int read = scanf(MAX_MSG_ELEM_SCANF, key1);
 		char key2[MAX_MSG_ELEM_SIZE + 1];
 		read += scanf(MAX_MSG_ELEM_SCANF, key2);
+		pps_value_t value_get;	
 
 		if (read == 2) {
-			pps_value_t value_get1;
-			pps_value_t value_get2;
-			debug_print("The key1 : %s",key1);
-			debug_print("The key2 : %s",key2);
-			error_code error1 = network_get(*client, key1, &value_get1);
-			error_code error2 = network_get(*client, key2, &value_get2);
-			debug_print("get1 : %s", value_get1);
-			debug_print("get2 : %s", value_get2);
-			if (error1 != 0 && error2 != 0) {
+			error_code error1 = network_get(*client, key1, &value_get);
+			strcpy(value1, value_get);
+			error_code error2 = network_get(*client, key2, &value_get);
+			strcpy(value2, value_get);
+			if (error1 != 0 || error2 != 0) {
 				printf("FAIL\n");
 				return 0;
 			}
-			debug_print("get1 : %ld",&value_get1);
-			debug_print("get2 : %ld",&value_get2);
-			debug_print("Searched string : %s",value_get1);
-			debug_print("string searched for : %s",value_get2);
-			const char * location = strstr(value_get1,value_get2);
+			const char * location = strstr(value1,value2);
 			if(location == NULL){
-				puts("-1");
+				fprintf(stdout,"OK -1\n");
 			}else{
 				debug_print("String start at location : %s",location);
-				fprintf(stdout,"OK %ld\n",value_get1-location);
+				fprintf(stdout,"OK %ld\n",location - value1);
 			}
-			// 	error_code error = network_put(*client, key, value);
-		// 	if (error != 0) {
-		// 		printf("FAIL\n");
-		// 	} else {
-		// 		printf("OK\n");
-		// 	}
 		}
 
 		while (!feof(stdin) && !ferror(stdin) && getc(stdin) != '\n');
